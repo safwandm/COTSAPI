bfb5a2e544cb5b5cc49cf1482b435d0cde3ed789
safwandm <safwan.damanik@gmail.com>
2024-12-20 06:03:08 +0700

    567

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,13 @@ Route::middleware('auth:sanctum')->get('/protected', function () {
     return response()->json(['message' => 'This is a protected route'], 200);
 });
 
+use App\Http\Controllers\GenericCrudController;
+// ->where('model', 'motors|transaksis|ulasans')
+
+Route::prefix('{model}')->group(function () {
+    Route::get('/', [GenericCrudController::class, 'index']);
+    Route::get('/{id}', [GenericCrudController::class, 'show']);
+    Route::post('/', [GenericCrudController::class, 'store']);
+    Route::put('/{id}', [GenericCrudController::class, 'update']);
+    Route::delete('/{id}', [GenericCrudController::class, 'destroy']);
+});
